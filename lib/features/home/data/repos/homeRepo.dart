ba6e46d8d9ebd61628b42790b9a9ce0f8ecbf7fe
Abderraouf Benbellat <rbenballat@gmail.com>
2023-10-14 00:03:51 +0100

    create eaither class

--- lib/features/home/data/repos/homeRepo.dart
+++ lib/features/home/data/repos/homeRepo.dart
@@ -1,8 +1,12 @@
 
 
+import 'package:bookapp/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
    
-    fetchNewsetBooks();
-    fetchFeaturedBooks();
+  Future<Either<dynamic,List<BookModel>>> fetchNewsetBooks();
+  Future<Either<dynamic,List<BookModel>>> fetchFeaturedBooks();
 
 }
+
