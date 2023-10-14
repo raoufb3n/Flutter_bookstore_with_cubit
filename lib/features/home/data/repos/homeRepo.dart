5b180cf62d5b3a9247f02fee886182b4e5332cbb
Abderraouf Benbellat <rbenballat@gmail.com>
2023-10-14 13:31:42 +0100

    create failure class and implement  in homeRepo

--- lib/features/home/data/repos/homeRepo.dart
+++ lib/features/home/data/repos/homeRepo.dart
@@ -1,12 +1,13 @@
 
 
+import 'package:bookapp/core/errors/errors.dart';
 import 'package:bookapp/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
    
-  Future<Either<dynamic,List<BookModel>>> fetchNewsetBooks();
-  Future<Either<dynamic,List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 
 }
 
